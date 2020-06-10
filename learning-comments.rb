793ae357f1382ed986ab2f249f7ee28bf15b021b
Jonathan Tan <jonathantan@brandeis.edu>
2020-06-10 14:59:26 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,22 @@
+#run_code_inside = false
+#puts "Code before if...end" 
+#if run_code_inside 
+#  puts "code inside"
+#end 
+#puts "Code after if...end"
+
+#chance_of_rain = 0.2
+#puts  "Let's go outside!"
+#if chance_of_rain >0.5 
+ # puts "pack an umbrella!"
+#else puts "enjoy the fine day"
+#end 
+#puts "oh, and always wear sunscreen"
+
+
+magic_exit_number = 7
+count = 0
+while count < 10 && count != magic_exit_number do
+  puts "I am the #{count}, I love to count!" # Work
+  count = count + 1
+end
